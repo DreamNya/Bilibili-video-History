a86cf3939e312b9502255de73396bfa670070f4c
DreamNya <34838824+DreamNya@users.noreply.github.com>
2021-09-04 20:28:25 +0800

    更新V1.1
    a.修复视频选集bug
    b.增加视频选集多P独立观看信息记录
    c.现在bilibili视频页内点击相关视频跳转页面也能正常记录观看信息了（*感谢DevSplash大佬提供的方法）
    d.若干细小优化
    e.更换jQuery CDN

--- Bilibili-video-History.user.js
+++ Bilibili-video-History.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Bilibili视频观看历史记录
 // @namespace    Bilibili-video-History
-// @version      1.0
-// @require      https://code.jquery.com/jquery-3.5.1.min.js#sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=
+// @version      1.1
+// @require      https://cdn.jsdelivr.net/npm/jquery@latest/dist/jquery.min.js
 // @description  记录并提示Bilibili已观看或已访问但未观看视频记录
 // @author       DreamNya
 // @downloadURL	 https://github.com/DreamNya/Bilibili-video-History/raw/main/Bilibili-video-History.user.js
@@ -48,14 +48,21 @@
 (4)、如配合【Bilibili Evolved】亦可在关注动态中提示简略观看记录
 
 已知问题：
-水平有限，至今未弄懂bilibili视频页内相关视频跳转是怎么做到的"无损"跳转的，故通过相关视频跳转后的页面不刷新脚本直接记录可能存在bug
 (未公开的初版脚本仅记录了观看类型并未记录观看百分比等其他内容，由于数据过多，作者不想放弃，为了兼容旧版数据，故存在一些公开发布版本用不到的代码，以后有空再优化。)
 
 更新计划：
 增加脚本可视化操作面板，开放部分自定义设置功能，开放历史记录列表（目前仅能从Tampermonkey脚本-存储中手动查看）
-优化顶部导航栏中收藏、历史
+优化顶部导航栏中收藏、历史等css
+优化小标签代码层面方法
 
 更新记录：
+V1.1(2021-9-4):
+a.修复视频选集bug
+b.增加视频选集多P独立观看信息记录
+c.现在bilibili视频页内点击相关视频跳转页面也能正常记录观看信息了（*感谢DevSplash大佬提供的方法）
+d.若干细小优化
+e.更换jQuery CDN
+
 v1.0(2021-8-28):
 a.首次公开发布
 */
@@ -64,7 +71,7 @@ a.首次公开发布
 (function() {
     'use strict';
     main()
-    function main(){
+    function main(init=true){
         let BV
         let BV记录
         let BV类型
@@ -73,42 +80,43 @@ a.首次公开发布
         let 观看时长
         let 总时长
         let 观看百分比
-        let mark=false
+        let mark=$("[class='bilibili-player-iconfont']").attr("aria-label")=="暂停"?true:false
+        let 标题
 
         获取当前页面()
         if (页面类型=="video"){
             if(BV类型){
-                views(BV类型,观看时长,观看百分比,BV时间)
+                views(BV类型,观看时长,观看百分比,BV时间,BV)
             }else{
-                GM_setValue(BV,["已访问",,,time(),document.title])
+                GM_setValue(BV,["已访问",,,time(),标题])
             }
 
-            let 播放绑定计时器=setInterval(()=>{
-                if($("bwp-video").length>0){
-                    clearInterval(播放绑定计时器)
-                    $("bwp-video").on('play',()=>{
-                        记录观看()
-                    })
+            if(init){
+                let 播放绑定计时器=setInterval(()=>{
+                    if($("bwp-video").length>0){
+                        clearInterval(播放绑定计时器)
+                        $("bwp-video").on('play',()=>{
+                            记录观看()
+                        })
+                    }
+                    if($("video").length>0){
+                        clearInterval(播放绑定计时器)
+                        $("video").on('play',()=>{
+                            记录观看()
+                        })
+                    }
+                },100)
                 }
-                if($("video").length>0){
-                    clearInterval(播放绑定计时器)
-                    $("video").on('play',()=>{
-                        记录观看()
-                    })
-                }
-            },100)
-
-            //$("a[href]").filter(function(){return $(this).attr("href").indexOf("/video/BV")==0}).on("click",()=>{window.location=window.location})
 
             window.onbeforeunload = function () {
                 switch(BV类型){
                     case "已观看":
                         if(mark==true){
-                            记录观看()
+                            记录观看(true)
                         }
                         break
                     case "已访问":
-                        GM_setValue(BV,["已访问",,,time(),document.title])
+                        GM_setValue(BV,["已访问",,,time(),标题])
                 }
             }
         }
@@ -116,53 +124,115 @@ a.首次公开发布
         if (true){
             let times=0
             let timer=setInterval(()=>{
-                $("a[href]").filter(
-                    function(){
-                        let href=$(this).attr("href")
-                        if(href.indexOf("BV")>-1 || href.indexOf("av")>-1){
-                            return $(this).find("img").length>0
+                $("a[href]").filter(function(){
+                    let href=$(this).attr("href")
+                    if((href.indexOf("BV")>-1 || href.indexOf("av")>-1)){
+                        return $(this).find("img").length>0 && $(this).parents(".list-box").length==0
+                    }
+                }).each(function(){
+                    if($(this).prev().css("z-index")==108){
+                        $(this).prev().remove()
+                    }else if($(this).find(".video-preview").children().eq(0).css("z-index")==108){
+                        $(this).find(".video-preview").children().eq(0).remove()
+                    }
+                    let href=$(this).attr("href").split("?")[0].split("/")
+                    let i
+                    for(i=0; i<href.length;i++){
+                        if(href[i].indexOf("BV")>-1 || href[i].indexOf("av")>-1){
+                            break
                         }
-                    }).each(
-                    function(){
-                        if($(this).prev().css("z-index")==108){
-                            $(this).prev().remove()
-                        }else if($(this).find(".video-preview").children().eq(0).css("z-index")==108){
-                            $(this).find(".video-preview").children().eq(0).remove()
+                    }
+                    let text=GM_getValue(href[i])
+                    if(text){
+                        let 状态=text[0]
+                        let 百分比=text[2]
+                        if($(this).parents(".van-popper").length==0){
+                            $(this).before(小标签(百分比?状态+百分比:状态)) //Bilibili Evolved
+                        }else{
+                            $(this).find(".video-preview").prepend(小标签(百分比?状态+百分比:状态)) //原版
                         }
-                        let href=$(this).attr("href").split("?")[0].split("/")
-                        let i
-                        for(i=0; i<href.length;i++){
-                            if(href[i].indexOf("BV")>-1 || href[i].indexOf("av")>-1){
-                                break
-                            }
-                        }
-                        let text=GM_getValue(href[i])
-                        if(text){
-                            let 状态=text[0]
-                            let 百分比=text[2]
-                            if($(this).parents(".van-popper").length==0){
-                                $(this).before(小标签(百分比?状态+百分比:状态)) //Bilibili Evolved
-                            }else{
-                                $(this).find(".video-preview").prepend(小标签(百分比?状态+百分比:状态)) //原版
-                            }
-                        }
-                    })
+                    }
+                })
                 times++
                 if(times>=600){
                     clearInterval(timer)
                 }
             },1000)
             }
+        /*let observer2 = new MutationObserver(function(m){
+            m.forEach(function(e){
+                if(e.target.nodeName=="A"){
+                    let href=$(e.target).attr("href").split("?")[0].split("/")
+                    let i
+                    for(i=0; i<href.length;i++){
+                        if(href[i].indexOf("BV")>-1 || href[i].indexOf("av")>-1){
+                            break
+                        }
+                        if(i==href.length-1){
+                            return
+                        }
+                    }
+                    if($(e.target).find("img").length>0 && $(e.target).parents(".list-box").length==0){
+                        let text=GM_getValue(href[i])
+                        if(text){
+                            let 状态=text[0]
+                            let 百分比=text[2]
+                            if($(e.target).parents(".van-popper").length==0){
+                                $(e.target).before(小标签(百分比?状态+百分比:状态)) //Bilibili Evolved
+                            }else{
+                                $(e.target).find(".video-preview").prepend(小标签(百分比?状态+百分比:状态)) //原版
+                            }
+                        }
+                    }
+                }
+            })
 
-        function 记录观看(){
+        })
+
+        observer2.observe(document, {
+            //childList: true,
+            attributes: true,
+            subtree: true
+        })*/
+
+        let observer = new MutationObserver(function (m) {
+            m.forEach(function(e){
+                e.removedNodes.forEach(function(ee){
+                    let x=$(ee).find(".bilibili-player-video-time-now")
+                    let y=$(ee).find(".bilibili-player-video-time-total")
+                    if(x.length>0 && y.length>0){
+                        if(BV类型=="已观看" && mark==true){
+                            观看时长=x.text()
+                            总时长=y.text()
+                            观看百分比=Math.round((观看时长.split(":")[0]*60+观看时长.split(":")[1]*1)/(总时长.split(":")[0]*60+总时长.split(":")[1]*1)*100)+"%"
+                            GM_setValue(BV,[BV类型,观看时长,观看百分比,time(),标题])
+                        }
+                    }
+                    if($(ee).attr("class")=="bui-select-wrap"){ //先这样凑合用吧
+                        $("#view").remove()
+                        setTimeout(main(false),2000)
+                    }
+                })
+            })
+        });
+
+        function 记录观看(final=false){
+            if(mark==false){
+                observer.observe($("#bilibili-player")[0], {
+                    childList: true,
+                    subtree: true
+                })
+            }
             获取当前页面()
             获取观看百分比()
             mark=true
             BV类型="已观看"
-            GM_setValue(BV,[BV类型,观看时长,观看百分比,time(),document.title])
+            if(final){
+                GM_setValue(BV,[BV类型,观看时长,观看百分比,time(),标题])
+            }
         }
         function 获取当前页面(){
-            BV=getBV(window.location.pathname,2)
+            BV=getBV(window.location.href,4).split("?spm")[0].replace("?p=1","")
             BV记录=GM_getValue(BV)
             if(BV记录){
                 BV类型=BV记录[0]
@@ -170,7 +240,8 @@ a.首次公开发布
                 观看百分比=BV记录[2]
                 BV时间=BV记录[3]
             }
-            页面类型=getBV(window.location.pathname,1)
+            页面类型=getBV(window.location.href,3)
+            标题=document.title
         }
 
         function 获取观看百分比(){
@@ -190,19 +261,21 @@ a.首次公开发布
             </div>`
         }
 
-        function views(BV类型_,观看时长,观看百分比,BV时间){
+        function views(BV类型_,观看时长,观看百分比,BV时间,BV号){
             let 时长
             if (观看时长){
                 时长=`<br>${观看时长}(${观看百分比})`
-        }else{
-            时长=``
+                BV号=BV号+"&#10;左键单击跳转视频播放进度&#10;右键单击删除视频记录信息"
+            }else{
+                时长=``
+            BV号=BV号+"&#10;右键单击删除视频记录信息"
         }
             if (BV时间){
                 BV时间=`<p style="margin:0 10px 5px 10px">${BV时间.split(" ")[0]}<br>${BV时间.split(" ")[1]}</p>`
         }else{
             BV时间=``
         }
-            $("body").append(`<div id="view" style="position:fixed;bottom:15px;left:15px;text-align:center;border-left:6px solid #2196F3;background-color: #aeffff;font-family:'Segoe UI','Segoe','Segoe WP','Helvetica','Tahoma','Microsoft YaHei','sans-serif';font-weight:666">
+            $("body").append(`<div id="view" title=${BV号} style="position:fixed;bottom:15px;left:15px;text-align:center;border-left:6px solid #2196F3;background-color: #aeffff;font-family:'Segoe UI','Segoe','Segoe WP','Helvetica','Tahoma','Microsoft YaHei','sans-serif';font-weight:666">
         <p style="margin:5px 10px 5px 10px">${BV类型_}${时长}</p>
         ${BV时间}
         </div>`)
@@ -221,10 +294,11 @@ a.首次公开发布
             $("#view").bind("contextmenu", ()=>{
                 return false
             })
-            $("#view").mousedown((e)=>{
+            $("#view").mousedown(function(e){
                 if(e.which==3){
                     GM_deleteValue(BV)
                     BV类型="已删除"
+                    $(this).html($(this).html().replace(/已访问/,"已删除").replace(/已观看/,"已删除"))
                 }
             })
         }
