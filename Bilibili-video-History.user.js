5e0e4b9683b14df4949df71208c98157e128f6ca
DreamNya <34838824+DreamNya@users.noreply.github.com>
2021-10-05 08:05:49 +0800

    更新V1.2
    a.脚本现在支持搜索页面
    b.优化小标签代码
    c.新增多P小标签信息提示（现在悬浮在小标签上会显示所有分P观看信息）
    d.修复观看百分比计算bug及观看时间跳转bug
    e.修复偶尔不记录观看信息bug（可能仍存在问题）
    f.若干细小优化

--- Bilibili-video-History.user.js
+++ Bilibili-video-History.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bilibili视频观看历史记录
 // @namespace    Bilibili-video-History
-// @version      1.1
+// @version      1.2
 // @require      https://cdn.jsdelivr.net/npm/jquery@latest/dist/jquery.min.js
 // @description  记录并提示Bilibili已观看或已访问但未观看视频记录
 // @author       DreamNya
@@ -15,6 +15,7 @@
 // @match        https://www.bilibili.com/?*
 // @match        https://www.bilibili.com/account/history
 // @match        https://www.bilibili.com/watchlater/*
+// @match        https://search.bilibili.com/*
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        GM_deleteValue
@@ -51,11 +52,19 @@
 (未公开的初版脚本仅记录了观看类型并未记录观看百分比等其他内容，由于数据过多，作者不想放弃，为了兼容旧版数据，故存在一些公开发布版本用不到的代码，以后有空再优化。)
 
 更新计划：
-增加脚本可视化操作面板，开放部分自定义设置功能，开放历史记录列表（目前仅能从Tampermonkey脚本-存储中手动查看）
+增加脚本可视化操作面板，开放部分自定义设置功能，开放历史记录列表（目前仅能从Tampermonkey脚本-存储中手动查看）（计划下个版本更新）
 优化顶部导航栏中收藏、历史等css
-优化小标签代码层面方法
+进一步优化小标签代码层面方法（计划用MutationObserver方法，可能有点困难）
 
 更新记录：
+V1.2(2021-10-5):
+a.脚本现在支持搜索页面
+b.优化小标签代码
+c.新增多P小标签信息提示（现在悬浮在小标签上会显示所有分P观看信息）
+d.修复观看百分比计算bug及观看时间跳转bug
+e.修复偶尔不记录观看信息bug（可能仍存在问题）
+f.若干细小优化
+
 V1.1(2021-9-4):
 a.修复视频选集bug
 b.增加视频选集多P独立观看信息记录
@@ -70,6 +79,7 @@ a.首次公开发布
 
 (function() {
     'use strict';
+    let record_p=GM_listValues().filter(i=>i.indexOf("?p=")>-1)
     main()
     function main(init=true){
         let BV
@@ -84,7 +94,8 @@ a.首次公开发布
         let 标题
 
         获取当前页面()
-        if (页面类型=="video"){
+
+        if (页面类型=="video" && BV){
             if(BV类型){
                 views(BV类型,观看时长,观看百分比,BV时间,BV)
             }else{
@@ -105,36 +116,43 @@ a.首次公开发布
                             记录观看()
                         })
                     }
-                },100)
-                }
+                },100)}
+
+            if(mark){
+                记录观看()
+            }
 
             window.onbeforeunload = function () {
+                console.log("onbeforeunload"+BV类型)
                 switch(BV类型){
                     case "已观看":
-                        if(mark==true){
-                            记录观看(true)
-                        }
+                        mark && 记录观看(true)
                         break
                     case "已访问":
-                        GM_setValue(BV,["已访问",,,time(),标题])
+                        BV && GM_setValue(BV,["已访问",,,time(),标题])
+                        break
+                    case "已删除":
+                        break
+                    default:
+                        console.log(BV类型)
+                        //return false
                 }
             }
-        }
 
+        }
         if (true){
             let times=0
             let timer=setInterval(()=>{
+                times++
+                if(times>=600){ //600秒停止对比记录 防止页面崩溃 待优化
+                    clearInterval(timer)
+                }
                 $("a[href]").filter(function(){
                     let href=$(this).attr("href")
                     if((href.indexOf("BV")>-1 || href.indexOf("av")>-1)){
                         return $(this).find("img").length>0 && $(this).parents(".list-box").length==0
                     }
                 }).each(function(){
-                    if($(this).prev().css("z-index")==108){
-                        $(this).prev().remove()
-                    }else if($(this).find(".video-preview").children().eq(0).css("z-index")==108){
-                        $(this).find(".video-preview").children().eq(0).remove()
-                    }
                     let href=$(this).attr("href").split("?")[0].split("/")
                     let i
                     for(i=0; i<href.length;i++){
@@ -143,20 +161,48 @@ a.首次公开发布
                         }
                     }
                     let text=GM_getValue(href[i])
+                    let href_p=record_p.filter(item=>(item.indexOf(href[i])>-1))
                     if(text){
-                        let 状态=text[0]
-                        let 百分比=text[2]
-                        if($(this).parents(".van-popper").length==0){
-                            $(this).before(小标签(百分比?状态+百分比:状态)) //Bilibili Evolved
+                        href_p.unshift(href[i])
+                    }else if(href_p.length>0){
+                        text=GM_getValue(href_p[0])
+                    }else{
+                        return
+                    }
+                    let 状态=text[0]
+                    let 百分比=text[2] || ""
+                    let 时间=text[3]
+                    let 文本=href_p.length>1 ? "已记录 多P" : 状态+百分比
+
+                    if($(this).prev().css("z-index")==108){
+                        if($(this).prev().text()!=文本){
+                            $(this).prev().remove()
                         }else{
-                            $(this).find(".video-preview").prepend(小标签(百分比?状态+百分比:状态)) //原版
+                            return
+                        }
+                    }else if($(this).find(".video-preview").children().eq(0).css("z-index")==108){
+                        if($(this).find(".video-preview").children().eq(0).text()!=文本){
+                            $(this).find(".video-preview").children().eq(0).remove()
+                        }else{
+                            return
                         }
                     }
+
+                    if(href_p.length>1){
+                        时间+=(href_p[0].indexOf("?")>-1?" "+href_p[0].split("?")[1].replace("=","").replace("p","P")+" ":" P1 ")+状态+百分比
+                        href_p.splice(0,1)
+                        href_p.forEach(item=>{
+                            let item_value=GM_getValue(item)
+                            item_value[2]=item_value[2] || ""
+                            时间+=("&#10;"+item_value[3]+" "+item.split("?")[1].replace("=","").replace("p","P")+" "+item_value[0]+item_value[2])
+                        })
+                    }
+                    if($(this).parents(".van-popper").length==0 && 文本){
+                        $(this).before(小标签(文本,时间)) //Bilibili Evolved
+                    }else{
+                        $(this).find(".video-preview").prepend(小标签(文本,时间)) //原版
+                    }
                 })
-                times++
-                if(times>=600){
-                    clearInterval(timer)
-                }
             },1000)
             }
         /*let observer2 = new MutationObserver(function(m){
@@ -201,14 +247,16 @@ a.首次公开发布
                     let x=$(ee).find(".bilibili-player-video-time-now")
                     let y=$(ee).find(".bilibili-player-video-time-total")
                     if(x.length>0 && y.length>0){
-                        if(BV类型=="已观看" && mark==true){
+                        if(BV类型=="已观看" && mark==true && BV){
                             观看时长=x.text()
                             总时长=y.text()
-                            观看百分比=Math.round((观看时长.split(":")[0]*60+观看时长.split(":")[1]*1)/(总时长.split(":")[0]*60+总时长.split(":")[1]*1)*100)+"%"
+                            观看百分比=Math.round(观看时长.split(":").reverse().map((item,index)=>(item*=Math.pow(60,index))).reduce((total,item)=>(total+=item))/总时长.split(":").reverse().map((item,index)=>(item*=Math.pow(60,index))).reduce((total,item)=>(total+=item))*100)+"%"
                             GM_setValue(BV,[BV类型,观看时长,观看百分比,time(),标题])
                         }
                     }
                     if($(ee).attr("class")=="bui-select-wrap"){ //先这样凑合用吧
+                        //console.log(ee)
+                        //console.log(e)
                         $("#view").remove()
                         setTimeout(main(false),2000)
                     }
@@ -227,12 +275,16 @@ a.首次公开发布
             获取观看百分比()
             mark=true
             BV类型="已观看"
-            if(final){
+            if(final && BV){
                 GM_setValue(BV,[BV类型,观看时长,观看百分比,time(),标题])
             }
         }
         function 获取当前页面(){
-            BV=getBV(window.location.href,4).split("?spm")[0].replace("?p=1","")
+            try {
+                BV=/((BV|bv)[A-Za-z0-9]+(\?p=[0-9]+)?)|(av\d+(\?p=[0-9]+)?)/g.exec(window.location.href)[0].replace("?p=1","")
+            }catch(e){
+
+            }
             BV记录=GM_getValue(BV)
             if(BV记录){
                 BV类型=BV记录[0]
@@ -247,7 +299,7 @@ a.首次公开发布
         function 获取观看百分比(){
             观看时长=$(".bilibili-player-video-time-now").text()
             总时长=$(".bilibili-player-video-time-total").text()
-            观看百分比=Math.round((观看时长.split(":")[0]*60+观看时长.split(":")[1]*1)/(总时长.split(":")[0]*60+总时长.split(":")[1]*1)*100)+"%"
+            观看百分比=Math.round(观看时长.split(":").reverse().map((item,index)=>(item*=Math.pow(60,index))).reduce((total,item)=>(total+=item))/总时长.split(":").reverse().map((item,index)=>(item*=Math.pow(60,index))).reduce((total,item)=>(total+=item))*100)+"%"
             return 观看百分比
         }
 
@@ -255,10 +307,8 @@ a.首次公开发布
             return path.split("/")[num]
         }
 
-        function 小标签(text,line_height=19){
-            return `<div style="position: absolute;margin: .5em;padding: 0 5px;height: 20px;line-height: ${line_height}px;border-radius: 4px;color: #fff;font-style: normal;background-color: rgb(122 134 234 / 70%);z-index:108;">
-            ${text}
-            </div>`
+        function 小标签(text,time,line_height=20){
+            return `<div title="${time}" style="position: absolute;margin: .5em;padding: 0 5px;height: 20px;line-height: ${line_height}px;border-radius: 4px;color: #fff;font-style: normal;font-size: 12px;background-color: rgb(122 134 234 / 70%);z-index:108;">${text}</div>`
         }
 
         function views(BV类型_,观看时长,观看百分比,BV时间,BV号){
@@ -269,7 +319,7 @@ a.首次公开发布
             }else{
                 时长=``
             BV号=BV号+"&#10;右键单击删除视频记录信息"
-        }
+                }
             if (BV时间){
                 BV时间=`<p style="margin:0 10px 5px 10px">${BV时间.split(" ")[0]}<br>${BV时间.split(" ")[1]}</p>`
         }else{
@@ -282,11 +332,11 @@ a.首次公开发布
             if (观看时长){
                 $("#view").on("click",()=>{
                     if($("bwp-video").length>0){
-                        $("bwp-video")[0].currentTime=观看时长.split(":")[0]*60+观看时长.split(":")[1]*1
+                        $("bwp-video")[0].currentTime=观看时长.split(":").reverse().map((item,index)=>(item*=Math.pow(60,index))).reduce((total,item)=>(total+=item))
                         $("bwp-video")[0].play()
                     }
                     if($("video").length>0){
-                        $("video")[0].currentTime=观看时长.split(":")[0]*60+观看时长.split(":")[1]*1
+                        $("video")[0].currentTime=观看时长.split(":").reverse().map((item,index)=>(item*=Math.pow(60,index))).reduce((total,item)=>(total+=item))
                         $("video")[0].play()
                     }
                 })
@@ -298,7 +348,7 @@ a.首次公开发布
                 if(e.which==3){
                     GM_deleteValue(BV)
                     BV类型="已删除"
-                    $(this).html($(this).html().replace(/已访问/,"已删除").replace(/已观看/,"已删除"))
+                    $(this).html($(this).html().replace(/已访问|已观看/,"已删除"))
                 }
             })
         }
